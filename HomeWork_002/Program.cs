e4e62342eb9bd6f3d40b6948f6ff6edda49a6527
Lapets_Dmitry <veles-hj@mail.ru>
2023-01-27 21:01:23 +0300

    Решил задачу № 4

---
+++ HomeWork_002/Program.cs
@@ -0,0 +1,25 @@
+﻿int.TryParse (Console.ReadLine(), out int a);
+int.TryParse (Console.ReadLine(), out int b);
+int.TryParse (Console.ReadLine(), out int c);
+if ((a < b) & (b < c) | (b < a) & (a < c))
+{
+Console.Write("max = ");
+Console.WriteLine(c);
+}
+
+else if ((a < c) & (c < b) | (c < a) & (a < b))
+{
+Console.Write("max = ");
+Console.WriteLine(b);
+}
+
+else if ((c < b) & (b < a) | (b < c) & (c < a))
+{
+Console.Write("max = ");
+Console.WriteLine(a);
+}
+
+else
+{
+    Console.WriteLine("Числа равны");
+}
