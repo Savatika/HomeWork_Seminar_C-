22463437d7a3868d423a68b0645c1c6ef5248226
Lapets_Dmitry <veles-hj@mail.ru>
2023-01-28 10:35:52 +0300

    добавил коментарий к задаче 6

--- HomeWork_003/Program.cs
+++ HomeWork_003/Program.cs
@@ -1,3 +1,4 @@
-﻿int.TryParse(Console.ReadLine(), out int a);
+﻿// определение четного числа
+int.TryParse(Console.ReadLine(), out int a);
 if ((a%2) == 0) Console.WriteLine("Yes");
 else Console.WriteLine("No");
