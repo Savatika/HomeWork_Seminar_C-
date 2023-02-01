0256e8237d6bf1e3a185df87b2c47ffac1961d95
Lapets_Dmitry <veles-hj@mail.ru>
2023-02-01 20:32:18 +0300

    решил задачу 10

---
+++ HomeWork_005/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите трехзначное число");
+int.TryParse(Console.ReadLine(), out int N);
+Console.WriteLine((N/10)%10);
+
