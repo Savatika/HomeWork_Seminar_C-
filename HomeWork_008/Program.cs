73e7cbf243a85cb1cc181c2d2e51d2e0a542d62e
Lapets_Dmitry <veles-hj@mail.ru>
2023-02-07 20:07:47 +0300

    решил задачу 19

---
+++ HomeWork_008/Program.cs
@@ -0,0 +1,15 @@
+﻿// Является ли пятизначное число полиндромом?
+Console.WriteLine("Введите пятизначное число");
+int.TryParse (Console.ReadLine(), out int N);
+int number1 = N / 10000;
+int number2 = (N / 1000) % 10;
+int number4 = (N%100) / 10;
+int number5 = N % 10;
+if ((number1 == number5) && (number2 == number4))
+{
+    Console.WriteLine("Число является палиндромом");
+}
+else
+{
+    Console.WriteLine("Число не палиндром");
+}
