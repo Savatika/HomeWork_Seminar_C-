84eec092ae8d871bcda499831b350856bbfbd828
Lapets_Dmitry <veles-hj@mail.ru>
2023-02-18 16:36:34 +0300

    Решил задачи семинара 6

---
+++ HomeWork_Seminar6/Task_41/Program.cs
@@ -0,0 +1,85 @@
+﻿// сколько чисел больше нуля ввел пользоваетль
+
+Console.WriteLine("Сколько чисел вы хотите ввести?");
+
+int length = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите числа");
+
+int [] array = creationArray(length);
+int summ = sumPosNumber(array);
+
+Console.WriteLine(string.Join(" ", array));
+Console.WriteLine($"Количество положительных чисел - {summ}");
+
+int [] creationArray(int length)
+{
+    int [] arr = new int[length];
+    for(int i = 0; i < length; i++)
+    {
+        arr[i] = int.Parse(Console.ReadLine());
+    }
+    return arr;
+}
+
+int sumPosNumber(int [] arr)
+{
+    int summ = 0;
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] >= 0)
+        {
+            summ++;
+        }
+    }
+    return summ;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int summDig(int number)
+// {
+//     bool c = true;
+//     int summ = 0;
+//     while(c)
+//     {
+//         int num = ReadInt("number");
+//         if (num > 0)
+//         {
+//             summ++;
+//         }
+//         else if (num == 0000)
+//         {
+//             c = false;
+//         }
+//     }
+//     return summ;
+// }
+
---
+++ HomeWork_Seminar6/Task_43/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+int k1 = int.Parse(Console.ReadLine());
+int k2 = int.Parse(Console.ReadLine());
+int b1 = int.Parse(Console.ReadLine());
+int b2 = int.Parse(Console.ReadLine());
+
+double deltaK = k2 - k1;
+double deltaB = b2 - b1;
+double x = (-1) * (deltaB/deltaK);
+
+double y = k1 * x + b1;
+
+Console.WriteLine($"Точка пересечения прямых - ({x}, {y})");
